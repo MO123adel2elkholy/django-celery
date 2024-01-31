d88799d27de9cc5e38479449174f89a7f90d4878
mahmoud adel elkholy <adel333mahmoud@gmail.com>
2024-02-01 01:29:29 +0200

    adding celery to our project

---
+++ core/celery.py
@@ -0,0 +1,19 @@
+
+import os
+from celery import Celery
+from datetime import timedelta
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+app = Celery('core')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+app.conf.timezone = 'Europe/London'
+
+app.conf.beat_schedule = {
+    "every_thirty_seconds": {
+        "task": "users.tasks.thirty_second_func",
+        "schedule": timedelta(seconds=30),
+    },
+}
+
+app.autodiscover_tasks()
